7e5494db0281f6f3661117420ee8ebf18bfddbf2
FannyRoux <fannyrouxfr@gmail.com>
2025-09-16 00:00:12 +0200

    finishing homepage mobile mode css

--- wp-content/themes/chiffonnierTheme/footer.php
+++ wp-content/themes/chiffonnierTheme/footer.php
@@ -1,11 +1,11 @@
-<footer id="mon-footer" class="blue-background place-width">
-    <p>&copy; <?php echo date('Y'); ?> 
-        <span class = "chiffonnier upper">Chiffonnier</span> 
-        <span class = "enchante upper"> enchanté </span> 
-        <span> Tous droits réservés. | 
-            <a href="<?php echo esc_url( get_permalink(491) ); ?>" class="mentions-legales-lien">Mentions légales</a>
-        </span>
-    </p>
+<footer id="mon-footer" class="place-width">
+    <div>&copy; <?php echo date('Y'); ?> 
+        <span class = "chiffonnier-upper">Chiffonnier</span> 
+        <span class = "enchante-upper"> enchanté </span> 
+        <div class="mentions-legales"> Tous droits réservés. &nbsp | &nbsp
+            <a href="<?php echo esc_url( get_permalink(491) ); ?>">Mentions légales</a>
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
