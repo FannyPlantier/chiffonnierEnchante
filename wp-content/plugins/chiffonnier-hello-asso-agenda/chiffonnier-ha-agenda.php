f7bf6b7c69fc4d6317dafb75c1ae5b89f24d09c2
Fanny <fannyrouxfr@gmail.com>
2025-11-04 14:37:58 +0100

    create : base for personnal HelloAsso API plugin

---
+++ wp-content/plugins/chiffonnier-hello-asso-agenda/chiffonnier-ha-agenda.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: HelloAsso Agenda API
+Description: Récupère et affiche les événements HelloAsso via l'API.
+Version: 1.0
+Author: Fanny Plantier
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
